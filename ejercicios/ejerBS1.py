0c113d587b067fa955a22e521a17e75ee4d6a571
pabperort1 <pabperort1@alum.us.es>
2023-02-22 20:27:35 +0100

    Primer botón casi terminado

--- ejercicios/ejerBS1.py
+++ ejercicios/ejerBS1.py
@@ -1,27 +1,54 @@
 from bs4 import BeautifulSoup
 import urllib.request 
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+import os, ssl
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-# import os, ssl
-# if (not os.environ.get("PYTHON"))
 """almacenar de cada vino: nombre, precio,origen,bodega,tipo_uva"""
-def abrir_url():
-    url = "https://www.vinissimus.com/es/vinos/tinto/?cursor=0"
-    f = urllib.request.urlopen(url)
-    s = BeautifulSoup(f,"lxml")
-    return s
+def abrir_urls():
+    lista = []
+    for i in range(0,3):
+        multiplicacion = 36*i
+        url = f"https://www.vinissimus.com/es/vinos/tinto/?cursor={multiplicacion}"
+        f = urllib.request.urlopen(url)
+        s = BeautifulSoup(f,"lxml")
+        list_una_pag = s.find_all("div",class_="product-list-item")
+        lista.extend(list_una_pag)
+    return lista
 
 def cargar():
-    s = abrir_url()
-    list_una_pag = s.find_all("div",class_="product-list-item")
-    
+    list_una_pag = abrir_urls()
     for vino in list_una_pag:
         detalles=vino.find("div",class_="details")
-        
         nombre=detalles.find("a")["title"]
         bodega=detalles.find("div",class_="cellar-name").string
         region=detalles.find("div",class_="region").string
-        tipo_uva=list(detalles.find("div",class_="tags").stripped_strings)
-        print("".join(tipo_uva))
+        lista_tipos_uva=list(detalles.find("div",class_="tags").stripped_strings)
+        tipos_uva = "".join(lista_tipos_uva)
         
+
+def first_window():
+    app = Tk()
+    app.title("Vino app")
+    menu = Menu(app)
+    
+    datamenu = Menu(menu, tearoff = 0)
+    menu.add_cascade(label="Datos", menu= datamenu) 
+    datamenu.add_command(label = "Cargar",command=cargar)
+    datamenu.add_command(label = "Listar")
+    datamenu.add_command(label = "Salir",command=app.quit)
+    
+    # listmenu= Menu(menu,tearoff=0)
+    # menu.add_cascade(label="Listar",menu=listmenu)
+    # listmenu.add_command(label = "Completo",command=list_data)
+    # listmenu.add_command(label = "Ordenado",command=listar_ordenado)
+
+    app.config(menu=menu)
+    app.mainloop()
+
 if __name__ == "__main__":
-    cargar()
+    first_window()
