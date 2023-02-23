322bfc987fa4e84fcaac2da21fe023b59f34e9db
pabperort1 <pabperort1@alum.us.es>
2023-02-23 01:28:35 +0100

    Almacenar en la base de datos ejercicio 1 BS hecho

--- ejercicios/ejerBS1.py
+++ ejercicios/ejerBS1.py
@@ -21,16 +21,30 @@ def abrir_urls():
     return lista
 
 def cargar():
+    con = sqlite3.connect("VinosDB.sqlite")
+    con.execute("DROP TABLE IF EXISTS VINOS")
+    con.execute("""CREATE TABLE VINOS (NAME TEXT, PRICE REAl, ORIGIN TEXT, WINE_CELLAR TEXT,
+    GRAPE_TYPE TEXT)""")
+    cursor = con.cursor()
+
     list_una_pag = abrir_urls()
     for vino in list_una_pag:
         detalles=vino.find("div",class_="details")
+        precio = list(vino.find("p",class_=["price"]).stripped_strings)[0]
+        dto = vino.find("p",class_=["price"]).find_next_sibling("p",class_="dto")
         nombre=detalles.find("a")["title"]
         bodega=detalles.find("div",class_="cellar-name").string
         region=detalles.find("div",class_="region").string
         lista_tipos_uva=list(detalles.find("div",class_="tags").stripped_strings)
         tipos_uva = "".join(lista_tipos_uva)
-        
-
+        if dto:
+            precio = list(dto.stripped_strings)[0]
+        cursor.execute("INSERT INTO VINOS (NAME,PRICE,ORIGIN,WINE_CELLAR,GRAPE_TYPE) VALUES (?,?,?,?,?)", (nombre,precio,region,bodega,tipos_uva))
+        con.commit()
+    num_vinos = cursor.execute("SELECT COUNT(*) FROM VINOS")
+    messagebox.showinfo("Base Datos", "Base de datos creada correctamente \nHay " + str(num_vinos.fetchone()[0])+ " número de vinos")
+    cursor.close()
+    con.close()
 def first_window():
     app = Tk()
     app.title("Vino app")
